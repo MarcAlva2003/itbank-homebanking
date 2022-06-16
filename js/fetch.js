19647432e0d359b009a366ccf100ef5cdf3cea27
faelias20 <fexeelias@gmail.com>
2022-06-16 20:34:14 -0300

    contenedorDolar

--- js/fetch.js
+++ js/fetch.js
@@ -15,7 +15,20 @@
 const contenedorDolares = document.getElementById('tets');
 
 function imprimirValores(dolar){
-    
+    const nombreDolar = document.createElement('span');
+    nombreDolar.innerHTML = dolar.nombre;
+
+    const precioVenta = document.createElement('p');
+    precioVenta.innerHTML = dolar.venta;
+
+    const contenedorDolar = document.createElement('div')
+
+    contenedorDolar.className = 'carta'
+
+    contenedorDolar.appendChild(nombreDolar)
+    contenedorDolar.appendChild(precioVenta)
+
+    contenedorDolares.appendChild(contenedorDolar)
 }
 
 
