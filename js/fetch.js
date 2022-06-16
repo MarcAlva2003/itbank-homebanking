55e3f9e512f0d0d8227c7d2e3716f0a3dcc55195
Marcos Alvarez <alvarez2003marcos@gamil.com>
2022-06-16 19:55:04 -0300

    Add: API call and iteration

--- js/fetch.js
+++ js/fetch.js
@@ -1,13 +1,32 @@
-const url = 'https://www.dolarsi.com/api/api.php?type=valoresprincipales'
+// const url = 'https://www.dolarsi.com/api/api.php?type=valoresprincipales'
 
-fetch(url)
-    .then(response => response.json() )
-    .then(data => {
+// fetch(url)
+//     .then(response => response.json() )
+//     .then(data => {
 
-        let element = document.getElementById('elemen')
-        element.innerHTML = '<p>${data.name}</p>'
+//         let element = document.getElementById('elemen')
+//         element.innerHTML = '<p>${data.name}</p>'
 
 
-        console.log(data)
-    })
-    .catch(err=>console.log(err))
+//         console.log(data)
+//     })
+//     .catch(err=>console.log(err))
+
+const contenedorDolares = document.getElementById('tets');
+
+function imprimirValores(dolar){
+    
+}
+
+
+function llamarApiDolares(){
+    fetch('https://www.dolarsi.com/api/api.php?type=valoresprincipales')
+        .then(res => res.json()) //SE PONE COMO PRIMER THEN
+        .then(res => { //RES = ARRAY DE OBJETOS
+            for(dolar of res){
+                imprimirValores(dolar.casa)
+            }
+        })
+}
+
+llamarApiDolares()
