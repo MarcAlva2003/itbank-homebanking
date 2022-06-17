ff786d87b5dcde49c50c305e2da64b32a846b761
pappafabian17 <fabianpappa2000@gmail.com>
2022-06-16 21:32:48 -0300

    FabianPappa Js Actualizado

--- js/fetch.js
+++ js/fetch.js
@@ -1,37 +1,47 @@
-const contenedorDolares = document.getElementById('dolars-container-javascrips');
-const textoUltimaFecha = document.getElementById('ult-fecha-dolar')
+const contenedorDolares = document.getElementById(
+  "dolars-container-javascrips"
+);
+const textoUltimaFecha = document.getElementById("ult-fecha-dolar");
 
-function imprimirValores(dolar){
-    const nombreDolar = document.createElement('span');
-    const precioVenta = document.createElement('p');
-    const precioCompra = document.createElement('p');
-    const contenedorDolar = document.createElement('div');
+function imprimirValores(dolar) {
+  const nombreDolar = document.createElement("span");
+  const precioVenta = document.createElement("p");
+  const precioCompra = document.createElement("p");
+  const variacion = document.createElement("p");
+  const contenedorDolar = document.createElement("div");
 
-    nombreDolar.innerHTML = dolar.nombre;
-    precioVenta.innerHTML = dolar.venta;    
-    precioCompra.innerHTML = dolar.compra;
+  nombreDolar.innerHTML = dolar.nombre;
+  precioVenta.innerHTML = `Venta: $ ${dolar.venta}`;
+  precioCompra.innerHTML = `Compra: $${dolar.compra}`;
+  variacion.innerHTML = `Variacion: ${dolar.variacion}%`;
 
-    contenedorDolar.className = 'carta';
+  contenedorDolar.className = "carta";
 
-    if(dolar.nombre !== 'Dolar Soja' && dolar.nombre !== 'Argentina' && dolar.nombre !== 'Bitcoin'){
-        contenedorDolar.appendChild(nombreDolar)
-        contenedorDolar.appendChild(precioVenta);
-        contenedorDolar.appendChild(precioCompra);
-        contenedorDolares.appendChild(contenedorDolar);
-    } else if(dolar.nombre === 'Argentina') {
-        textoUltimaFecha.innerHTML = `Ultima actualizacion: ${dolar.fecha}`;
-    }
+  if (
+    dolar.nombre !== "Dolar Soja" &&
+    dolar.nombre !== "Argentina" &&
+    dolar.nombre !== "Bitcoin"
+  ) {
+    contenedorDolar.appendChild(nombreDolar);
+    contenedorDolar.appendChild(precioVenta);
+    contenedorDolar.appendChild(precioCompra);
+    contenedorDolar.appendChild(variacion);
+
+    contenedorDolares.appendChild(contenedorDolar);
+  } else if (dolar.nombre === "Argentina") {
+    textoUltimaFecha.innerHTML = `Ultima actualizacion: ${dolar.fecha}`;
+  }
 }
 
-
-function llamarApiDolares(){
-    fetch('https://www.dolarsi.com/api/api.php?type=valoresprincipales')
-        .then(res => res.json()) //SE PONE COMO PRIMER THEN
-        .then(res => { //RES = ARRAY DE OBJETOS
-            for(dolar of res){
-                imprimirValores(dolar.casa);
-            }
-        })
+function llamarApiDolares() {
+  fetch("https://www.dolarsi.com/api/api.php?type=valoresprincipales")
+    .then((res) => res.json()) //SE PONE COMO PRIMER THEN
+    .then((res) => {
+      //RES = ARRAY DE OBJETOS
+      for (dolar of res) {
+        imprimirValores(dolar.casa);
+      }
+    });
 }
 
-    llamarApiDolares();
+llamarApiDolares();
